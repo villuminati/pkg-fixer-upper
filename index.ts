3eeb06f3b0f8e0a894e702d4f32ef294e49734cb
villuminati <rishi25m@gmail.com>
2024-07-12 22:15:54 +0530

    parse command line args in index.ts using minimist

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Run create-diff.ts or apply-diff.ts");
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+
+// console.debug({ argv });
+
+function main() {
+    if (argv._.length > 0) {
+        // createDiff
+        console.debug("case: create diff");
+    } else if (argv._.length === 0) {
+        // applyDiff
+        console.debug("case: apply diff");
+    }
+}
+
+main();
