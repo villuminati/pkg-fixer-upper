0ab6f6f013c0edb1965ac5041be983767c66c6db
villuminati <rishi25m@gmail.com>
2024-07-17 01:05:42 +0530

    applying patches (only for files at top level in dep, still need to work on going deeper in the tree)

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 import minimist from "minimist";
 import { createDiff } from "./src/create-diff";
+import { applyDiff } from "./src/apply-diff";
 
 const argv = minimist(process.argv.slice(2));
 
 // console.debug({ argv });
 
 function main() {
+    console.log("running pkg-fixer-upper ...");
     if (argv._.length > 0) {
         createDiff(argv._);
     } else if (argv._.length === 0) {
         // applyDiff
         console.debug("case: apply diff");
+        applyDiff();
     }
 }
 
--- src/apply-diff.ts
+++ src/apply-diff.ts
@@ -1,19 +1,70 @@
-import * as fs from "fs";
+// import * as fs from "fs";
 import * as Diff from "diff";
+import { readdir, stat } from "fs/promises";
+import { readFileSync, writeFileSync } from "fs";
+import path from "path";
 
-// Read the contents of text2.txt and diff.patch
-const text2 = fs.readFileSync("./data/text2.txt", "utf8");
-const patch = fs.readFileSync("./data/diff.patch", "utf8");
+const patchesFolder = process.env.PATCHES_DIR!;
 
-// Apply the patch to text2
-const result = Diff.applyPatch(text2, patch);
+async function listDepsToPatch() {
+    // list deps in patches directory
+    const entries = await readdir(patchesFolder, { withFileTypes: true });
+    const dirs = entries.filter((d) => d.isDirectory()).map((d) => d.name);
 
-if (result === false) {
-    console.error("Failed to apply patch", result);
-} else {
-    // Save the reconstructed text1.txt
-    fs.writeFileSync("./data/reconstructed_text1.txt", result, "utf8");
-    console.log(
-        "Patch applied successfully, reconstructed_text1.txt has been created"
-    );
+    console.log("dependencies that need to be patched: ", dirs);
+
+    return dirs;
+}
+
+export async function applyDiff() {
+    const depsToPatch = listDepsToPatch();
+    for (const dep of await depsToPatch) {
+        console.log("applying patch for", dep);
+
+        // TODO: At this point i am using all files will in one level deep.
+        // This is not ideal. We need to read nested dirs in the dep directory
+        const allFilesAndDirsInDep = await readdir(
+            path.join(patchesFolder, dep),
+            {
+                withFileTypes: true,
+            }
+        );
+
+        const filesToPatchInDep = allFilesAndDirsInDep
+            .filter((f) => f.isFile())
+            .map((f) => f.name);
+
+        console.log({ filesToPatchInDep });
+
+        for (const patchfile of filesToPatchInDep) {
+            console.log("applying patch for", patchfile);
+
+            const nameWithoutExtension = patchfile.replace(".patch", "");
+
+            // this text2 will be path to node_modules/dep/file.js
+            const text2 = readFileSync(
+                `./node_modules/${dep}/${nameWithoutExtension}`,
+                "utf8"
+            );
+            // this will be path to patches/dep/file.js.patch
+            const patch = readFileSync(`./patches/${dep}/${patchfile}`, "utf8");
+
+            // // Apply the patch to text2
+            const result = Diff.applyPatch(text2, patch);
+
+            if (result === false) {
+                console.error("Failed to apply patch", result);
+            } else {
+                // Save the reconstructed text1.txt to node_modules/dep/file.js again
+                writeFileSync(
+                    `./node_modules/${dep}/${nameWithoutExtension}`,
+                    result,
+                    "utf8"
+                );
+                console.log(
+                    `Patch applied successfully, /node_modules/${dep}/${nameWithoutExtension} has been created`
+                );
+            }
+        }
+    }
 }
